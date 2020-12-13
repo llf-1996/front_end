bbbbeef145ddc7eb3bb402f66b85ab82e694329e
llf <2367746876@qq.com>
2020-12-13 19:11:27 +0800

    深拷贝修改

--- js/deepCopy.js
+++ js/deepCopy.js
@@ -1,12 +1,22 @@
 (function () {
+  /**
+  * 深拷贝
+  */
   function deepCopy(data) {
     // 深拷贝
     var type = typeof data;
     var obj;
     if (type === 'object') {
-      obj = Array.isArray(data) ? [] : {};
-      for (var key in data) {
-        obj[key] = deepCopy(data[key]);
+      if (data === null) {
+        obj = null
+      } else if (data instanceof Date) {
+        // 时间类型
+        obj = data
+      } else {
+        obj = Array.isArray(data) ? [] : {};
+        for (var key in data) {
+          obj[key] = dcp(data[key]);
+        }
       }
     } else {
       obj = data;
